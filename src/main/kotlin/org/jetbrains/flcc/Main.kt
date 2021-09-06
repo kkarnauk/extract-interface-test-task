15e612e426ce1db270d205f7e89488094400ba2e
Kirill <bitikit@gmail.com>
2021-09-06 23:51:23 +0300

    Implement main

--- src/main/kotlin/org/jetbrains/flcc/Main.kt
+++ src/main/kotlin/org/jetbrains/flcc/Main.kt
@@ -1,11 +1,25 @@
 package org.jetbrains.flcc
 
 import org.jetbrains.flcc.cli.ArgsParser
+import org.jetbrains.flcc.cli.CliOptions
+import org.jetbrains.flcc.lang.MethodFilter
 
 fun main(args: Array<String>) {
-    val cliOptions = ArgsParser.parse(args.toList())
-    println(cliOptions)
-    // TODO options -> methods
-    // TODO methods -> interface
-    // TODO interface -> dump
+    val options = ArgsParser.parse(args.toList())
+    val classCode = options.input.path.toFile().apply { require(isFile) { "'inputPath' must be a file" } }.readText()
+    val methodFilter = options.requirements.toFilter()
+    val methods = options.input.language.extractMethods(classCode, options.input.name, methodFilter)
+    val interfaceCode = options.output.language.constructInterface(options.output.name, methods)
+    options.output.path.toFile().writeText(interfaceCode)
+}
+
+private fun CliOptions.MethodsRequirements.toFilter(): MethodFilter {
+    val whitelist = whitelist?.toSet()
+    val blacklist = blacklist?.toSet()
+    val accessModifier = accessModifier
+    return { method ->
+        method.accessModifier.name == accessModifier &&
+                (whitelist == null || method.name in whitelist) &&
+                (blacklist == null || method.name !in blacklist)
+    }
 }
